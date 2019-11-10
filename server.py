d4195487641f7a26c0be7bbafb53f66f860248c3
ZharkovMihail <56646565m@mail.ru>
2019-11-11 02:07:37 +0300

    not working

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 import json
-#import redis
+import redis
 from flask import Flask, request, Response, make_response
 
 app = Flask(__name__)
 
-d={}
+#d={}
 
 @app.route('/<key>/', methods=['GET', 'PUT', 'DELETE'])
 def requestic(key):
@@ -17,60 +17,65 @@ def requestic(key):
 		if key == None or message == None:
 			return Response(status = 400)
 		else:
-			d_key = d.get(key)
-			if d_key == None:
-				d.update({key: message})
-				return make_response({key: message}, 201)
-			else:
-				d.update({key: message})
-				return Response(status=200)
-			# cache = redis.Redis(host='rediska', port=6379)
-			# cache.ping()
-			# if not cache.exists(key):
-			# 	cache.set(key, json.dumps(message))
-			# 	return make_response({"key":"value"}, 201)
+			# d_key = d.get(key)
+			# if d_key == None:
+			# 	d.update({key: message})
+			# 	return make_response({key: message}, 201)
 			# else:
-			# 	cache.delete(key)
-			# 	cache.set(key, json.dumps(message))
-			# 	return Response(status = 200)
+			# 	d.update({key: message})
+			# 	return Response(status=200)
+			cache = redis.Redis(host='rediska', port=6379)
+			cache.ping()
+			if cache.exists(key):
+				cache.delete(key)
+				cache.set(key, json.dumps(message))
+				return Response(status = 200)
+			else:
+				cache.set(key, json.dumps(message))
+				return make_response({key:message}, 201)
+
 
 	elif request.method == "GET":
 		#key = data.get("key")
-		d_key = d.get(key)
-		if d_key == None:
-			return Response(status=404)
-		else:
-			return make_response({"message": d_key}, 200)
-		# cache = redis.Redis(host='rediska', port=6379)
-		# cache.ping()
-		# if cache.exists(key):
-		# 	res = json.loads(cache.get(key))
-		# 	return make_response({"message": res}, 200)
+
+		# d_key = d.get(key)
+		# if d_key == None:
+		# 	return Response(status=404)
 		# else:
-		# 	return Response(status=400)
+		# 	return make_response({"message": d_key}, 200)
+
+		cache = redis.Redis(host='rediska', port=6379)
+		cache.ping()
+		if cache.exists(key):
+			res = json.loads(cache.get(key))
+			return make_response({"message": res}, 200)
+		else:
+			return Response(status=400)
 
 	elif request.method == "DELETE":
 		#key = data.get("key")
-		if key == None:
-			return Response(status=400)
-		else:
-			d_key = d.get(key)
-			if d_key == None:
-				return Response(status=404)
-			else:
-				d.pop(key)
-				return make_response({"message": d_key}, 204)
-		# cache = redis.Redis(host='rediska', port=6379)
-		# cache.ping()
+
 		# if key == None:
-		# 	return Response(status = 400)
+		# 	return Response(status=400)
 		# else:
-		# 	if cache.exists(key):
-		# 		res = json.loads(cache.get(key))
-		# 		cache.delete(key)
-		# 		return make_response({"message": res}, 204)
-		# 	else:
+		# 	d_key = d.get(key)
+		# 	if d_key == None:
 		# 		return Response(status=404)
+		# 	else:
+		# 		d.pop(key)
+		# 		return make_response({"message": d_key}, 204)
+
+		cache = redis.Redis(host='rediska', port=6379)
+		cache.ping()
+		if key == None:
+			return Response(status = 400)
+		else:
+			if cache.exists(key):
+				res = json.loads(cache.get(key))
+				cache.delete(key)
+				return make_response({"message": res}, 204)
+			else:
+				return Response(status=404)
 
 if __name__ == '__main__':
 	app.run()
